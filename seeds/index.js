a67b6a0dd7ed46c53568adb70c675a1ebf531f40
Judit Ferenczik <jferenczik@gmail.com>
2021-12-05 20:55:19 +0100

    Every start is hard

---
+++ seeds/index.js
@@ -0,0 +1,42 @@
+
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    //useCreateIndex= true not supported
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error"));
+db.once("open", () => {
+    console.log("Dataase connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+
+        })
+        await camp.save();
+
+    }
+
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
