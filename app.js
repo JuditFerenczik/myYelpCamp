0ff8a14df89ad84fa0862c2c7a970abebde72413
Judit Ferenczik <jferenczik@gmail.com>
2022-01-02 19:53:08 +0100

    Error handling

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas.js');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
 //required for the put request  =>faking our express to treat it as a put request (it was POST)
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
@@ -25,53 +28,73 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
 
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+
+}
 
 app.get('/', (req, res) => {
     // res.send('Hello from YELPCAMP')
     res.render('home')
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
-});
+}));
 
 app.get('/campgrounds/new', (req, res) => {
     // const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid campground data', 400);
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
 
-});
+}));
 
 //order does matter here, if id is before new it wont work because it wont find anything with the id new
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
-});
-app.get('/campgrounds/:id/edit', async (req, res) => {
+}));
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-});
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     res.redirect(`/campgrounds/${campground._id}`)
 
-});
-app.delete('/campgrounds/:id', async (req, res) => {
+}));
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-});
+}));
 
-
+app.all('*', (req, res, next) => {
+    //res.send('UPSSS 404')
+    next(new ExpressError('Page not found', 404))
+})
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'OHHH noo!Something went wrong!'
+    res.status(statusCode).render('error', { err })
+})
 app.listen(3000, () => {
     console.log("Serving on port 3000")
 })
