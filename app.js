efcc997e85417741b65ea13bfe89d25bcf62c1d1
Judit Ferenczik <jferenczik@gmail.com>
2022-01-16 15:48:24 +0100

    one to many reviews

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
 //required for the put request  =>faking our express to treat it as a put request (it was POST)
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-
+const Review = require('./models/review');
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error"));
 db.once("open", () => {
-    console.log("Dataase connected");
+    console.log("Database connected");
 });
 const app = express();
 
@@ -31,6 +31,17 @@ app.use(methodOverride('_method'));
 const validateCampground = (req, res, next) => {
 
     const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+
+}
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
@@ -66,7 +77,8 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 //order does matter here, if id is before new it wont work because it wont find anything with the id new
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    //console.log(campground);
     res.render('campgrounds/show', { campground });
 }));
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
@@ -85,6 +97,22 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
 
 app.all('*', (req, res, next) => {
     //res.send('UPSSS 404')
